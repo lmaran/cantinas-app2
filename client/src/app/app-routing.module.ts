3ef2f1656103b9caca88dad4abc118d91fe82295
Lucian Maran <lucian.maran@outlook.com>
2018-03-11 21:49:03 +0200

    wip

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Link1Component } from './link1/link1.component';
+
+const routes: Routes = [{ path: 'link1', component: Link1Component }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { Link1Component } from './link1/link1.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, HelloWorldComponent, Link1Component],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BsDropdownModule.forRoot(),
+        TooltipModule.forRoot(),
+        ModalModule.forRoot(),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
